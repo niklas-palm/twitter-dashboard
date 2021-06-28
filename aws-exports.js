9343d7ff7b94f912b41a47715aa9083c08d60c63
robot <robot@example.com>
2021-06-28 22:21:53 +0200

    add config

---
+++ aws-exports.js
@@ -0,0 +1,10 @@
+const awsmobile = {
+  aws_project_region: "eu-west-1",
+  aws_cognito_identity_pool_id:
+    "eu-west-1:ec4e0a1f-a02b-47f3-94ec-eeb0b1f044c7",
+  aws_cognito_region: "eu-west-1",
+  aws_user_pools_id: "eu-west-1_yXjZRTDNM",
+  aws_user_pools_web_client_id: "7pq3uhnid3s3lgk1knicpsal44",
+};
+
+export default awsmobile;
--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Amplify from "aws-amplify";
+import {
+  AmplifyAuthenticator,
+  AmplifySignUp,
+  AmplifySignIn,
+} from "@aws-amplify/ui-react";
+import { AuthState, onAuthUIStateChange } from "@aws-amplify/ui-components";
+import awsconfig from "./aws-exports";
 
 import "./App.scss";
 
-function App() {
-  return (
+Amplify.configure(awsconfig);
+
+const App = () => {
+  const [authState, setAuthState] = useState();
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    onAuthUIStateChange((nextAuthState, authData) => {
+      setAuthState(nextAuthState);
+      setUser(authData);
+    });
+  }, []);
+
+  return authState === AuthState.SignedIn && user ? (
     <div className="AppContainer">
       <h1>Hello world!</h1>
     </div>
+  ) : (
+    <AmplifyAuthenticator>
+      <AmplifySignUp
+        slot="sign-up"
+        usernameAlias="email"
+        formFields={[{ type: "email" }, { type: "password" }]}
+      />
+      <AmplifySignIn slot="sign-in" usernameAlias="email" />
+    </AmplifyAuthenticator>
   );
-}
+};
 
 export default App;
