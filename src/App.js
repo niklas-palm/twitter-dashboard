e6e4e83cc3b10137206cdca8e84c221548d6b904
robot <robot@example.com>
2021-06-29 13:45:32 +0200

    AdC

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Amplify from "aws-amplify";
+import Amplify, { Auth } from "aws-amplify";
 import {
   AmplifyAuthenticator,
   AmplifySignUp,
@@ -7,10 +7,41 @@ import {
 } from "@aws-amplify/ui-react";
 import { AuthState, onAuthUIStateChange } from "@aws-amplify/ui-components";
 import awsconfig from "./aws-exports.js";
+import QuickSight from "aws-sdk/clients/quicksight";
 
 import "./App.scss";
 
 Amplify.configure(awsconfig);
+
+const getDashboardParams = {
+  AwsAccountId: "641758013508", // req
+  DashboardId: "c84f964d-f305-459e-8bc2-e6dc3802ee23", // req
+  IdentityType: "ANONYMOUS", // req
+};
+
+const getEmbedUrl = async () => {
+  const credentials = await Auth.currentCredentials();
+  console.log(credentials);
+
+  var quicksight = new QuickSight({
+    region: "eu-west-1",
+    credentials: {
+      accessKeyId: credentials.accessKeyId,
+      secretAccessKey: credentials.secretAccessKey,
+    },
+  });
+  try {
+    const resp = await quicksight
+      .getDashboardEmbedUrl(getDashboardParams)
+      .promise();
+    console.log(resp);
+  } catch (err) {
+    console.log("Error");
+    console.error(err);
+  }
+};
+
+getEmbedUrl();
 
 const App = () => {
   const [authState, setAuthState] = useState();
@@ -22,6 +53,8 @@ const App = () => {
       setUser(authData);
     });
   }, []);
+
+  console.log(user);
 
   return authState === AuthState.SignedIn && user ? (
     <div className="AppContainer">
